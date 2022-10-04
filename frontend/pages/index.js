7ac3fabd78866413d46c673e6e7089780bf6922a
Zzyy <72967431+ZhnYng@users.noreply.github.com>
2022-10-04 16:18:01 +0800

    Login page done

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,45 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Link from "next/link";
+import { auth } from "../firebase";
+import { useGlobalContext } from "../context";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 export default function Home(){
+  const { state, dispatch } = useGlobalContext();
+
+  React.useEffect(() => {
+    if(state.user){
+      location.href="/map"
+    }
+  }, [state.user])
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const response = await signInWithPopup(auth, provider);
+      console.log(response);
+      dispatch({ type: "set_user", payload: response.user });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return(
     <>
-      {/* <Navbar/> */}
       <main className="h-screen font-poppinsMedium flex flex-col justify-center items-center text-center" style={{backgroundColor: "rgb(172, 225, 138)"}}>
         <h1 className="text-8xl mb-6">Trash Go</h1>
-        <Link href="/camera">
-          <button type="button" class="text-4xl focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">START</button>
-        </Link>
+          <button 
+            onClick={(e) => {
+              e.preventDefault();
+              signInWithGoogle();
+            }}
+            type="button" class="text-4xl focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            START
+          </button>
       </main>
     </>
   )
