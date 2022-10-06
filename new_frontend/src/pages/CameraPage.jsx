df5d9e6e51d5b3bedac8560dc592b31a07ef2296
Elliott Chong <elliottchong16@gmail.com>
2022-10-06 19:34:44 +0800

    fix

--- new_frontend/src/pages/CameraPage.jsx
+++ new_frontend/src/pages/CameraPage.jsx
@@ -68,7 +68,7 @@ const CameraPage = () => {
         </Webcam>
         {popup && (
           <>
-            <div className="z-10 absolute top w-[80vw] top-1/2 flex flex-col gap-4 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-green-600 p-6 font-poppinsMedium rounded-lg">
+            <div className="z-10 absolute top max-h-[70vh] w-[80vw] top-1/2 flex flex-col gap-4 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-green-600 p-6 font-poppinsMedium rounded-lg">
               <h1 className="text-center font-bold text-2xl">{item}</h1>
               <img src={rectImg} alt="img" className="rounded-lg" />
               <p>
